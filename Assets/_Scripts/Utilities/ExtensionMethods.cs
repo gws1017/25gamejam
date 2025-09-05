daebe3ed832fc3345700b4266de943f2dbffff04
jaylynnmell <danmugi118@gmail.com>
2025-09-05 15:16:30 +0900

    Project Init)

---
+++ Assets/_Scripts/Utilities/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public static class ExtensionMethods
+{
+    public static void StartCoroutineHelper(this MonoBehaviour extension, ref Coroutine coroutineVariable, IEnumerator Coroutine)
+    {
+        if (coroutineVariable != null)
+            extension.StopCoroutine(coroutineVariable);
+
+        coroutineVariable = extension.StartCoroutine(Coroutine);
+    }
+}
