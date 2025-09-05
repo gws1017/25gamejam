09f7ba864f60aa0c04bf2f96dedcfaacd0a90bf8
jaylynnmell <danmugi118@gmail.com>
2025-09-05 17:02:38 +0900

    이자연 / 갈무리 폰트 업로드

---
+++ Assets/_Scripts/UI/UI_MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    [SerializeField] Button gameStartButton;
+    [SerializeField] Button exitButton;
+
+    private void Awake()
+    {
+        SubscribeOnClickEventListeners();
+    }
+
+    private void OnDestroy()
+    {
+        UnSubscribeOnClickEventListeners();
+    }
+
+    #region Internal Logic
+    private void SubscribeOnClickEventListeners()
+    {
+        gameStartButton.onClick.AddListener(() => SceneLoader.LoadScene(SceneLoader.Scene.GameScene));
+        exitButton.onClick.AddListener(() => Application.Quit());
+    }
+
+    private void UnSubscribeOnClickEventListeners()
+    {
+        gameStartButton.onClick.RemoveAllListeners();
+        exitButton.onClick.RemoveAllListeners();
+    }
+    #endregion
+}
---
+++ Assets/_Scripts/Utilities/SceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public enum Scene
+    {
+        Default,
+        MainMenuScene,
+        LoadingScene,
+        GameScene,
+    }
+
+    private static Scene targetScene = Scene.MainMenuScene;
+
+    public static void LoadScene(Scene targetScene)
+    {
+        // If the target scene is on Default, do nothing
+        if (targetScene == Scene.Default) return;
+
+        // Set the target scene to the static variable 'targetScene'
+        SceneLoader.targetScene = targetScene;
+
+        // Load the loading scene
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+
+        // Load the actual target scene in the SceneLoader_Callback method (which is in the LoadingScene)
+
+        // 01) Load the LoadingScene first
+        // 02) Then the LoadingScene will call the target scene in turn
+    }
+
+    public static void SceneLoader_Callback()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+
+        // Clear the target scene after loading
+        ClearTargetScene();
+    }
+
+    public static string GetTargetScene()
+    {
+        return targetScene.ToString();
+    }
+
+    private static void ClearTargetScene()
+    {
+        // Clear the target scene after loading
+        targetScene = Scene.Default; // Reset to a default scene
+    }
+}
