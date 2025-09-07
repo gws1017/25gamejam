c08f8b289ec22fe5d74cdaf25b496de8135288b2
jaylynnmell <danmugi118@gmail.com>
2025-09-07 14:08:54 +0900

    이자연 / Feat / GameOver구현

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,9 +11,6 @@ public class GameManager : MonoBehaviour
 
     private void SingleTon()
     {
-        if (Instance != null && Instance != this)
-            Destroy(gameObject);
-
         Instance = this;
     }
 
@@ -23,18 +21,32 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
+    [SerializeField] private Image clickBlocker;
+
     private float countdownToStartTimer = 3f;
     private bool isGamePaused = false;
 
-    private GameState currentGameState;
-    private enum GameState
+    public GameState currentGameState;
+    public enum GameState
     {
         CountdownToStart,
         GamePlaying,
         GameOver,
     }
 
-    public event EventHandler OnStateChanged;
+    public event EventHandler<OnStateChangedEventArgs> OnStateChanged;
+    public class OnStateChangedEventArgs
+    {
+        GameState currentGameState;
+
+        public OnStateChangedEventArgs(GameState newState)
+        {
+            this.currentGameState = newState;
+        }
+
+        public GameState GetCurrentGameState() => currentGameState;
+    }
+
     public event EventHandler OnGamePaused;
     public event EventHandler OnGameResumed;
 
@@ -43,6 +55,11 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         SingleTon();
+    }
+
+    private void Start()
+    {
+        SetState(GameState.CountdownToStart);
     }
 
     private void Update()
@@ -56,6 +73,13 @@ public class GameManager : MonoBehaviour
     }
 
     #region Public API
+    
+    [ContextMenu("GameOverState")]
+    public void GameOverState()
+    {
+        SetState(GameState.GameOver);
+    }
+
     public void PauseGame()
     {
         if (isGamePaused) return;
@@ -69,6 +93,7 @@ public class GameManager : MonoBehaviour
     public void PauseGameWithDelay()
     {
         this.StartCoroutineHelper(ref coroutine_PauseGame, PauseGameCoroutine());
+        Debug.Log("PauseGameWithDelay called.");
     }
 
     public void ResumeGame()
@@ -90,17 +115,14 @@ public class GameManager : MonoBehaviour
             case GameState.CountdownToStart:
                 StartCountDownTimer();
                 // 플레이어 슈팅 불가능
-                // UI 클릭 불가능
                 Debug.Log("Countdown To Start");
                 break;
             case GameState.GamePlaying:
                 // 플레이어 슈팅 가능
-                // UI 클릭 가능
                 // 적 스폰 시작
                 Debug.Log("Game Playing");
                 break;
             case GameState.GameOver:
-                // Handle Game Over Logic
                 break;
         }
     }
@@ -112,18 +134,46 @@ public class GameManager : MonoBehaviour
 
         // Timer hits zero, we start the game
         if (countdownToStartTimer < 0f)
-            ChangeState(GameState.GamePlaying);
+            SetState(GameState.GamePlaying);
     }
 
-    private void ChangeState(GameState newState)
+    private void SetState(GameState newState)
     {
         currentGameState = newState;
+        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs(newState));
+        ManagerActionsPerState(newState);
+    }
+
+    private void ManagerActionsPerState(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.CountdownToStart:
+                TurnOnClickBlocker();
+                break;
+            case GameState.GamePlaying:
+                TurnOffClickBlocker();
+                break;
+            case GameState.GameOver:
+                TurnOffClickBlocker();
+                break;
+        }
     }
 
     private IEnumerator PauseGameCoroutine()
     {
         yield return new WaitForSeconds(0.5f);
         PauseGame();
+    }
+
+    private void TurnOnClickBlocker()
+    {
+        clickBlocker.gameObject.SetActive(true);
+    }
+
+    private void TurnOffClickBlocker()
+    {
+        clickBlocker.gameObject.SetActive(false);
     }
     #endregion
 }
--- Assets/_Scripts/UI/UI_GameOver.cs
+++ Assets/_Scripts/UI/UI_GameOver.cs
@@ -1,32 +1,80 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_GameOver : MonoBehaviour, IToggleUI
 {
     [SerializeField] private GameObject contentParents;
-    [SerializeField] private Button exitButton;
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Button quitButton;
+
+    [Header("Button Tweeners")]
+    [SerializeField] DoTween_Button_Scale restartButtonTween;
+    [SerializeField] DoTween_Button_Scale quitButtonTween;
+
+    private void OnEnable()
+    {
+        GameManager.Instance.OnStateChanged += Instance_OnStateChanged;
+
+        SubscribeOnClickEvents();
+    }
+
+    private void OnDestroy()
+    {
+        UnsubscribeOnClickEvents();
+
+        if (GameManager.Instance == null) return;
+        
+        GameManager.Instance.OnStateChanged -= Instance_OnStateChanged;
+
+    }
+
+    private void Instance_OnStateChanged(object sender, GameManager.OnStateChangedEventArgs e)
+    {
+        if (e.GetCurrentGameState() == GameManager.GameState.GameOver)
+            Show();
+    }
 
     public void Hide()
     {
         contentParents.SetActive(false);
-
-        GameManager.Instance.ResumeGame();
     }
 
     public void Show()
     {
         contentParents.SetActive(true);
-
-        GameManager.Instance.PauseGameWithDelay();
     }
 
     public void SubscribeOnClickEvents()
     {
-    
+        quitButton.onClick.AddListener(() =>
+        {
+            quitButtonTween.OnButtonClick();
+
+            // Play Button Click SFX
+            SoundEvents.Instance.InvokeOnPlayButtonFx();
+
+            Hide();
+
+            // 메인메뉴로 이동
+            SceneLoader.LoadScene(SceneLoader.Scene.MainMenuScene);
+        });
+
+        restartButton.onClick.AddListener(() =>
+        {
+            restartButtonTween.OnButtonClick(); 
+
+            // Play Button Click SFX
+            SoundEvents.Instance.InvokeOnPlayButtonFx();
+            Hide();
+
+            // 게임씬 재로딩
+            SceneManager.LoadScene(SceneLoader.Scene.GameScene.ToString());
+        });
     }
 
     public void UnsubscribeOnClickEvents()
     {
-    
+        
     }
 }
